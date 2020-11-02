ae2435eae219cc985fd5c7555d4573991846168a
yilin xu <xu.yilin@husky.neu.edu>
2020-11-02 11:25:21 -0500

    create App.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'))
-
+ReactDOM.render(<App />, document.getElementById("root"));
