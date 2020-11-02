4624f7ee1a444726f1b837745bd6c5a2b889eccd
yilin xu <xu.yilin@husky.neu.edu>
2020-11-02 11:40:58 -0500

    fetch data from backend

--- src/App.js
+++ src/App.js
@@ -5,14 +5,26 @@ import dataService from "./services/data";
 const App = () => {
   const [data, setData] = useState([]);
 
-  useEffect(() => {
-    const d = dataService.getAll();
+  useEffect( async () => {
+    const d = await dataService.getAll();
     setData(d);
   }, []);
 
+  console.log(data);
+
   return (
     <div>
-      <p>Hello world</p>
+      <p>HFI Assessment</p>
+
+      <h2>Data</h2>
+      <ol>
+        {data.map((d) => (
+          <li key={d.id}>
+            {" "}
+            {d.name}, {d.gender}, {d.age}{" "}
+          </li>
+        ))}
+      </ol>
     </div>
   );
 };
